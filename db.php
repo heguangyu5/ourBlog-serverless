e646092be7feef783d6be70e698f22dd1457812a
heguangyu5 <heguangyu5@qq.com>
2017-02-08 15:23:00 +0800

    export sql

--- db.php
+++ db.php
@@ -1,7 +1,11 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;port=3306;dbname=ourblog;charset=utf8', 'root', '123456');
+    $db = new PDO(
+        'mysql:host=localhost;port=3306;dbname=ourblog;charset=utf8',
+        'ourblog',
+        'thisisourblog'
+    );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Connection failed: ' . $e->getMessage());
---
+++ sql/1.0.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE `ourblog`;
+
+USE `ourblog`;
+
+CREATE TABLE `category` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Linux'),
+(2, 'Apache'),
+(3, 'PHP'),
+(4, 'MySQL'),
+(5, 'Javascript'),
+(6, 'Misc');
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
+  `category` int(10) unsigned NOT NULL,
+  `title` varchar(500) NOT NULL,
+  `content` text NOT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`),
+  KEY `category` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(200) NOT NULL,
+  `password` char(32) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO `user` (`uid`, `email`, `password`) VALUES
+(1, 'heguangyu5@qq.com', '6abb1bd6591204f34d0479fc512c7316');
+
+CREATE USER ourblog@localhost IDENTIFIED BY 'thisisourblog';
+GRANT INSERT,UPDATE,DELETE,SELECT ON ourblog.* TO ourblog@localhost;
