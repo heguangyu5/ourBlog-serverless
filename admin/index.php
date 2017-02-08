b2d3c8e59835302af762426ffbd328705f47884d
heguangyu5 <heguangyu5@qq.com>
2017-02-08 11:02:14 +0800

    index ok

--- admin/index.php
+++ admin/index.php
@@ -5,4 +5,20 @@
     include __DIR__ . '/header.php';
 ?>
 
+<div class="text-lft">
+<?php
+    include __DIR__ . '/../db.php';
+    $stmt = $db->query("SELECT id, title FROM posts WHERE uid = $uid ORDER BY id DESC", PDO::FETCH_OBJ);
+    foreach ($stmt as $row):
+?>
+    <div class="mar-btm">
+        <?php echo htmlspecialchars($row->title); ?>
+        <div class="pull-right">
+            <a href="edit.php">编辑</a>
+            <a href="delete.php">删除</a>
+        </div>
+    </div>
+<?php endforeach; ?>
+</div>
+
 <?php include __DIR__ . '/footer.php'; ?>
