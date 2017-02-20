d0ff655a0a4448f332ab2e2425af64619a7ee924
heguangyu5 <heguangyu5@qq.com>
2017-02-20 09:49:01 +0800

    删除文章时把标签也一并删除

--- admin/delete.php
+++ admin/delete.php
@@ -14,10 +14,22 @@
         if (!$id) {
             throw new InvalidArgumentException('invalid id');
         }
+
+        include __DIR__ . '/../db.php';
+        if (!$db->query("SELECT id FROM posts WHERE id = $id AND uid = $uid")->fetch(PDO::FETCH_COLUMN)) {
+            throw new InvalidArgumentException('you cannot only delete your own post');
+        }
     } catch (InvalidArgumentException $e) {
         die('参数不对');
     }
 
-    include __DIR__ . '/../db.php';
-    $db->exec("DELETE FROM posts WHERE id = $id AND uid = $uid");
-    header('Location: ./index.php');
+    $db->beginTransaction();
+    try {
+        $db->exec("DELETE FROM posts WHERE id = $id");
+        $db->exec("DELETE FROM post_tag WHERE post_id = $id");
+        $db->commit();
+        header('Location: ./index.php');
+    } catch (Exception $e) {
+        $db->rollBack();
+        die('Server Error');
+    }
